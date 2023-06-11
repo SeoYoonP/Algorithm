d528276a6bd1ee281f3805e9712e71ff8c0de6a0
박서윤 <35479166+SeoYoonP@users.noreply.github.com>
2023-06-11 20:28:37 +0900

    Create 마지막두원소.java

---
+++ Programmers/JAVA/마지막두원소.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[num_list.length + 1];
+        
+        for(int i=0; i<num_list.length; i++) {
+            answer[i] = num_list[i];
+        }
+        
+        int lastIndex = num_list.length - 1;
+        
+        if (num_list[lastIndex] > num_list[lastIndex - 1]) {
+            answer[lastIndex + 1] = num_list[lastIndex] - num_list[lastIndex - 1];
+        } else {
+            answer[lastIndex + 1] = num_list[lastIndex] * 2;
+        }
+        return answer;
+    }
+}
