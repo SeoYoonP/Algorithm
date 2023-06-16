ddd04e8954594b3e63f8917c997916c6b1290b53
박서윤 <35479166+SeoYoonP@users.noreply.github.com>
2023-06-16 23:17:54 +0900

    Create 문자열을정수로바꾸기.java

---
+++ Programmers/JAVA/문자열을정수로바꾸기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        answer = Integer.parseInt(s);
+        return answer;
+    }
+    
+    public static void main(String[] args){
+        Solution strChangeInt = new Solution();
+        System.out.println(strChangeInt);
+    }
+}
