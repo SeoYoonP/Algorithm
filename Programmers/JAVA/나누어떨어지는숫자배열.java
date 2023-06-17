2237b889c3d87ca2ed67d77c35cdf0ca69c171f5
박서윤 <35479166+SeoYoonP@users.noreply.github.com>
2023-06-17 18:25:53 +0900

    Create 나누어떨어지는숫자배열.java

---
+++ Programmers/JAVA/나누어떨어지는숫자배열.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+class Solution {
+    public int[] solution(int[] arr, int divisor) {
+        List<Integer> list = new ArrayList<>();
+        
+        for(int i : arr){
+            if(i % divisor == 0){
+                list.add(i);
+            }
+        }
+        
+        if (list.isEmpty()) {
+            list.add(-1);
+        } else {
+            Collections.sort(list);
+        }
+
+        int[] answer = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+
+        return answer;
+    }
+}
